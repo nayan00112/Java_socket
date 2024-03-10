c72955bf08aa5fd40839cb5d5bacd05d761dff85
Nayan Patel <nayanpatel00112@gmail.com>
2024-03-10 18:36:47 +0530

    InetAddress

---
+++ InetAddress/Inet_01.java
@@ -0,0 +1,28 @@
+package InetAddress;
+
+import java.net.InetAddress;
+
+public class Inet_01 {
+    public static void main(String[] args) {
+        try {
+            InetAddress a = InetAddress.getByName("www.google.com");
+            System.out.println(a.getHostAddress());
+            System.out.println(a.getHostAddress());
+            System.out.println(a.isLoopbackAddress());
+            System.out.println(a.isMulticastAddress());
+            System.out.println(a.isAnyLocalAddress());
+            System.out.println(a.getAddress());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+// Output:
+// 142.250.182.196
+// 142.250.182.196
+// false
+// false
+// false
+// [B@3f99bd52
